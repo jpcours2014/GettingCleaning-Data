639c2aa2f35e53fd85580ec9b1ec87ba67b18256
jpcours2014 <jpcours2014@gmail.com>
2014-11-24 11:27:41 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,13 +15,13 @@ activity_labels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header
 
 ##Load training data
 X_train = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
-y_train = read.csv("UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE, col.names=c("ActivityId"))
-subject_train= read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE, col.names=c("Subject"))
+y_train = read.csv("UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE, col.names=c("Activity_Label"))
+subject_train= read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE, col.names=c("subject"))
 
 ##Load test data
 X_test = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-y_test = read.csv("UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE, col.names=c("ActivityId"))
-subject_test= read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE, col.names=c("Subject"))
+y_test = read.csv("UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE, col.names=c("Activity_Label"))
+subject_test= read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE, col.names=c("subject"))
 
 
 ## 1. Merges the training and the test sets to create one data set.
@@ -31,13 +31,13 @@ all_data <- rbind(train_data, test_data)
 
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-all_labels <- rbind(rbind(features, c(562,"Subject")),c(563, "ActivityId"))[,2]
+all_labels <- rbind(rbind(features, c(562,"subject")),c(563, "Activity_Label"))[,2]
 names(all_data) <- all_labels
 
-all_data<-all_data[,grepl("mean|std|Subject|ActivityId", names(all_data))]
+all_data<-all_data[,grepl("mean|std|subject|Activity_Labeld", names(all_data))]
 
 ## 3. Uses descriptive activity names to name the activities in the data set
-all_data <- join(all_data, activity_labels, by = "ActivityId", match = "first")
+all_data <- join(all_data, activity_labels, by = "Activity_Label", match = "first")
 all_data <- all_data[,-1]
 
 ## 4. Appropriately labels the data set with descriptive activity names.
@@ -62,5 +62,5 @@ colnames(all_data) = colNames;
 
 
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidy_data<-ddply(all_data, c("Subject","ActivityId"), numcolwise(mean))
+tidy_data<-ddply(all_data, c("subject","Activity_Label"), numcolwise(mean))
 write.table(tidy_data, "tidy_data.txt", sep="\t")
