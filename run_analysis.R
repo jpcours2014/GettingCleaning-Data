4d4f2530aea8b5aa1a2b5c337349af3653740228
jpcours2014 <jpcours2014@gmail.com>
2014-11-24 01:25:46 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,31 +6,33 @@
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ##Load features
-features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE, colClasses = c("character"))
 
 ##Load activity labels
-activity_Labels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+activity_labels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
 
 ##Load training data
 X_train = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
-Y_train = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
-subject_train= read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+y_train = read.csv("UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE, col.names=c("ActivityId"))
+subject_train= read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE, col.names=c("Subject"))
 
 ##Load test data
 X_test = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-Y_test = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
-subject_test= read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+y_test = read.csv("UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE, col.names=c("ActivityId"))
+subject_test= read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE, col.names=c("Subject"))
 
 
 ## 1. Merges the training and the test sets to create one data set.
-train_data <- cbind(cbind(X_train, subject_train), Y_train)
-test_data <- cbind(cbind(X_test, subject_test), Y_test)
+train_data <- cbind(cbind(X_train, subject_train), y_train)
+test_data <- cbind(cbind(X_test, subject_test), y_test)
 all_data <- rbind(train_data, test_data)
 
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-extracted_features <- grepl("mean|std", features)
-all_data<-all_data[,extracted_features]
+all_labels <- rbind(rbind(features, c(562,"Subject")),c(563, "ActivityId"))[,2]
+names(all_data) <- all_labels
+
+all_data<-all_data[,grepl("mean|std|Subject|ActivityId", names(all_data))]
 
 ## 3. Uses descriptive activity names to name the activities in the data set
 
