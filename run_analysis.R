cbd36974c5ecd4331871b751204c49275f8443cf
jpcours2014 <jpcours2014@gmail.com>
2014-11-24 01:41:19 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+require(plyr)
+
 ##  run_analysis.R that does the following:
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
@@ -60,5 +62,5 @@ colnames(all_data) = colNames;
 
 
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidy_data<-ddply(all_data, c("Subject","Activity"), numcolwise(mean))
+tidy_data<-ddply(all_data, c("Subject","ActivityId"), numcolwise(mean))
 write.table(tidy_data, "tidy_data.txt", sep="\t")
